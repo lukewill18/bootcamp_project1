ec887d648465c86dba81a9b42be4e546f4de9efc
lukewill18 <lukewill18@gmail.com>
2018-07-10 10:01:31 -0700

    first draft of page

---
+++ script.js
@@ -0,0 +1,6 @@
+function initMap() {
+    var portal = {lat: 33.6509104, lng: -117.838332};
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 15, center: portal});
+    var marker = new google.maps.Marker({position: portal, map: map});
+}
